0f0de5e716b36af93b9b83ea1b6669cf4f136317
agarapunaveen <67690657+agarapunaveen@users.noreply.github.com>
2025-01-28 17:06:47 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,21 @@
-resource "aws_instance" "terraform-server" {
+/*resource "aws_instance" "terraform-server" {
   ami           = "ami-0e2c8caa4b6378d8c"
   instance_type    = var.instance_type
 
   tags = {
     Name = "instance-1"
+  }
+} */
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "single-instance"
+
+  instance_type          = var.instance_type
+  
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
   }
 }
