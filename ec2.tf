5ac0d6c6c810a7e8962904766ac69ddb53d8b1f9
agarapunaveen <67690657+agarapunaveen@users.noreply.github.com>
2025-01-28 09:43:50 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform-server" {
   ami           = "ami-0e2c8caa4b6378d8c"
-  instance_type    = lookup(var.instance_type,terraform.workspace,"t2.micro")
+  instance_type    = var.instance_type
 
   tags = {
     Name = "instance-1"
